14b0091293b576442590ed3336b40b169870a2ef
Kioh Han <kioh.han@disney.com>
2020-01-14 17:35:36 -0500

    Update uiReducer.js

--- src/redux/reducers/uiReducer.js
+++ src/redux/reducers/uiReducer.js
@@ -2,12 +2,17 @@ import {
   SET_ERRORS,
   CLEAR_ERRORS,
   LOADING_UI,
-  STOP_LOADING_UI
+  STOP_LOADING_UI,
+  SET_MESSAGES,
+  ADD_MESSAGE,
+  DELETE_MESSAGE,
+  CLEAR_MESSAGES
 } from "../types";
 
 const initialState = {
   loading: false,
-  errors: null
+  errors: null,
+  messages: []
 };
 
 export default function(state = initialState, action) {
@@ -33,6 +38,28 @@ export default function(state = initialState, action) {
       return {
         ...state,
         loading: false
+      };
+    case SET_MESSAGES:
+      return {
+        ...state,
+        messages: action.payload
+      };
+    case CLEAR_MESSAGES:
+      return {
+        ...state,
+        messages: []
+      };
+    case ADD_MESSAGE:
+      return {
+        ...state,
+        messages: [...state.messages, action.payload]
+      };
+    case DELETE_MESSAGE:
+      return {
+        ...state,
+        messages: state.messages.filter(
+          message => message.id !== action.payload
+        )
       };
     default:
       return state;
